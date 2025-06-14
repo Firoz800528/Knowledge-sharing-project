135d59fa0f8a1e6db8526b5c0431c30c57de432f
Firozzaman <firoz800528@gmail.com>
2025-06-15 03:48:05 +0600

    tenth commit

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-// tailwind.config.js
-module.exports = {
-  darkMode: 'class', // Enables dark mode via class (not media)
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  plugins: [require('daisyui')],
-};
